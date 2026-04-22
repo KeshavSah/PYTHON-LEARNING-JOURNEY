238448537d9b40635e5649b2fe5c35390f8cad59
KeshavSah <sanjukshah@gmail.com>
2026-04-22 18:42:33 +0545

    Add files via upload

---
+++ 01-Basics/Currency_calculator.py
@@ -0,0 +1,6 @@
+pesos = int(input('Colombian pesos:' ))
+soles = int(input('Peruvian soles:' ))
+reais = int(input('Brazillian reais:' ))
+
+Total = pesos * 0.00028 + soles * 0.29 + reais * 0.20
+print(Total)
