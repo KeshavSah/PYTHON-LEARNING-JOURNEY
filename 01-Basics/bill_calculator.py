9603db63eea9b521481d1d54007dcbb6a3592f0f
KeshavSah <sanjukshah@gmail.com>
2026-04-23 16:43:40 +0545

    Add files via upload

---
+++ 01-Basics/bill_calculator.py
@@ -0,0 +1,7 @@
+price = float(input("Enter the price: "))
+quantity = int(input("Enter the quantity: "))
+
+total = price * quantity
+
+print("------------------------")
+print("Grand Total: ", total)
